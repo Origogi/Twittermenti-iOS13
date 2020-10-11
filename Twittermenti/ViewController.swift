fbac5b20fb7b4c6bb66f67c4687c756c1a1b0d2e
김정태(Jeongtae Kim)/App개발팀/11ST <origogi.kim@sk.com>
2020-10-11 19:53:14 +0900

    Implement test code to check if Twitter API works properly

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -7,16 +7,25 @@
 //
 
 import UIKit
+import SwifteriOS
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
+    
+    let swifter = Swifter(consumerKey: "FEAZwnygkUcFqppAG1oaTOk2J", consumerSecret: "4HQxlYjPcagwDHxrQ9hB7RTDmlawxPl03DpHQmZvWpxLYNZd3R")
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        swifter.searchTweet(using: "@Apple", success: { (results, metadata) in
+            print(results)
+        }) { (error) in
+            print(error)
+        }
     }
 
     @IBAction func predictPressed(_ sender: Any) {
